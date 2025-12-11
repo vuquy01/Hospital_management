7d1e3302015a3e54ec47972a5d7818d3f77f027e
vuquy01 <137639706+vuquy01@users.noreply.github.com>
2025-12-11 14:16:28 +0700

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,9 +1,9 @@
 #pragma once
 #include"Employee.h"
-#include"Schedule.h"
 #include"Person.h"
 class Doctor :public Person, Employee
 {
+	static const int BENH_NHAN_TOI_DA = 10;
 	/*Khối 1 thuộc tính chuyên môn*/
 	string chungChiHanhNghe;      // Số chứng chỉ hành nghề
 	string Chuyenkhoa;
@@ -15,32 +15,13 @@ public:
 	void Input();
 	void Display();
 	void hienThiThongTinCoBan() const;
+	string taoID();
 
 	/*khoi ham gan gia tri*/
-	void ganMaSoThue(const string& ma);
-	void ganMaBacSi(const string& id);
 	void ganChuyenKhoa(const string& khoa);
 
 	/*khoi ham lay gia tri*/
-	string layMaSoThue() const;
-	string layMaBacSi() const;
 	string layChuyenKhoa() const;
-
-	/*khoi ham quan ly benh nhan*/
-	void themBenhNhanDieuTri(const string& maBN, const string& chanDoan);
-	void xoaBenhNhanDieuTri(const string& maBN);
-	void capNhatTinhTrangBenhNhan(const string& maBN, const string& tinhTrang);
-	void timKiemBenhNhan(const string& maBN) const;
-
-	/*khoi ham quan ly lich lam viec*/
-	void themLichLamViec(const string& ngay, const string& ca);
-	void huyLichLamViec(const string& ngay);
-	bool kiemTraLichTrong(const string& ngay, const string& ca) const;
-	void huyLichTaiKham(int index);
-	void danhDauDaTaiKham(int index);
-	void themLichHen(const string& maLichHen, const string& ngay, const string& gio);
-	bool kiemTraLichTrung(const string& ngay, const string& gio) const;
-	void hienThiLichLamViec() const;
 
 	/*khoi ham thong ke va bcao*/
 	float tinhDoanhThuThang(int thang, int nam) const;//thong ke
