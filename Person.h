ac28556f96fc9de947c7cad759e7208595dfb9a8
vuquy01 <137639706+vuquy01@users.noreply.github.com>
2025-12-11 13:23:44 +0700

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,43 +1,66 @@
 #pragma once
-
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using std::string;
-using std::ifstream;
-using std::ofstream;
-using std::cout;
-
-class Person {
-private:
-    string Name;
-    string date;
-    string CCCD_CMND;
-    string GioiTinh;
-    string DiaChi;
-    string SDT;
-    int age;
+#pragma once
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+/*Lớp cơ sỏ trìu tượng*/
+class Person
+{
+protected:
+	/*khoi thong tin co ban*/
+	string IDPerson;
+	string CCCD_CMND;      // Số căn cước công dân/chứng minh nhân dân
+	string Name;
+	string date;/*viet ham tinh tuoi*/
+	int age;
+	string GioiTinh; /*gioi han gioi tinh lai xong ktra*/
+	string DiaChi;
+	string SDT; /*Hàm ktra*/
+	string mail; /*Hàm ktra*/
 public:
-    Person();
-    Person(string cccd, string n, string d, string sex, string diachii, string sdt); 
-    void Display();
-    void hienThiThongTinCoBan();
-    string layName();
-    string layDate();
-    string laySdt();
-    string layDiachi();
-    void ganDate(string d);
-    void ganTen(string ten);
-    void ganSDT(string sdt);
-    void ganGioiTinh(string sex);
-    int tinhTuoi();
-    bool kiemTraSDTHopLe();
-    bool kiemTraTuoiHopLe();
-    bool kiemTraCCCDHopLe();
-    bool kiemTraGioiTinhHopLe();
-    bool kiemTraNgaySinhHopLe();
-    void luuVaoFile(ofstream& file);
-    void docTuFile(ifstream& file); 
+	/*khoi ham khoi tao*/
+	Person();
+	Person(string cccd, string n, string d, int tuoi, string sex, string diachii, string sdt, string m);
+
+	/*khối các phương thức thuần ảo*/
+	virtual void Input();
+	virtual void Display();
+	virtual void hienThiThongTinCoBan() const;
+	virtual string taoID();
+
+	/*khối hàm lấy dữ liệu an toàn(const để không thay đổi đối tượng)*/
+	string layName() const;
+	string layDate() const;
+	int layAge() const;
+	string laySdt() const;
+	string layDiachi() const;
+	string layMail() const;
+
+	/*khối hàm gán giá trị*/
+	void ganMail(string m);
+	void ganTen(string ten);
+	void ganSDT(string sdt);
+	void ganDate(string d);
+	void ganGioiTinh(string sex);
+	void ganCCCD_CMND(string cccd);
+	void ganDiaChi(string adress);
+	void ganID();
+
+	/*khoi ham tinh gia tri*/
+	int tinhTuoi() const;              // Tính tuổi từ ngày sinh
+
+	/*khối hàm ktra*/
+	bool kiemTraSDTHopLe() const;     // Kiểm tra định dạng số điện thoại
+	bool kiemTraTuoiHopLe() const;    // Kiểm tra tuổi hợp lý (1-150)
+	bool kiemTraEmailHopLe() const;    // Kiểm tra định dạng email
+	bool kiemTraNgaySinhHopLe() const; // Kiểm tra ngày sinh hợp lệ
+	bool kiemTraGioiTinhHopLe() const; // Kiểm tra giới tính (Nam/Nu)
+	bool kiemTraCCCDHopLe() const;
+	bool kiemTraDinhDangNgaySinh(string ngay) const;
+
+	/*khối hàm thao tác với file*/
+	void luuVaoFile(ofstream& file) const;    // Lưu vào file
+	void docTuFile(ifstream& file);           // Đọc từ file
 };
 
